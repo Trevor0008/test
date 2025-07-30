7b500e5991539d7eca999a50a760dd88e0b21398
Trevor Job <trevorbaraka43@gmail.com>
2025-07-30 14:44:02 +0300

    Third commit

---
+++ test3.js
@@ -0,0 +1,17 @@
+const readline=require('readline')
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+rl.question("Enter a number: ", function(num1){{
+    num1=parseInt(num1)
+
+    rl.question("Enter another number: ", function(num2){
+        num2=parseInt(num2)
+
+        let num3=num1*num2
+        console.log(num3)
+
+        rl.close()
+    })
+}})
